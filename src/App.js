d8eb6a770fa78f88d4d3dee2f7a91bc282829930
SAN-2816 <jinsan2816@gmail.com>
2019-10-08 16:05:55 +0900

    이미지 넣음

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import Fpeeps from './Images/4peeps.jpg';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <h1>
+        WANDERLAND
+      </h1>
+      <Menu/>
+      <Image/>
+    </div>
+  );
+}
+
+function Menu(){
+  return (
+    <div>
+      <li>HOME</li>
+      <li>ABOUT</li>
+    </div>
+  );
+}
+function Image(){
+  return(
+    <div>
+      <img src={Fpeeps} alt={"Fpeeps"}/>
     </div>
   );
 }
